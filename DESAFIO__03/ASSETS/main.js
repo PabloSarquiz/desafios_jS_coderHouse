01156d6bafc02d052d25755c4e064f8ea7ffe1c1
Pablo Sarquiz <pablogsarquiz@gmail.com>
2021-09-23 09:46:57 -0300

    W.I.P

--- DESAFIO__03/ASSETS/main.js
+++ DESAFIO__03/ASSETS/main.js
@@ -1,5 +1,9 @@
 multiplicando = parseInt(prompt("Imprimir la tabla del:"));
 
 for (i = 1; i <= 10; i++) {
+
+    alert(`${multiplicando}*${i} = ` + (multiplicando * i));
     console.log(`${multiplicando}*${i} = ` + (multiplicando * i));
+
 }
+
