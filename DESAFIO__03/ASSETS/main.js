faf29bfa6aae6c96e2aa5516ae66f1e3cbf69212
Pablo Sarquiz <pablogsarquiz@gmail.com>
2021-10-05 12:36:50 -0300

    W.I.P

--- DESAFIO__03/ASSETS/main.js
+++ DESAFIO__03/ASSETS/main.js
@@ -1,4 +1,11 @@
+
+
+
+
+
+do {
 multiplicando = parseInt(prompt("Que numero te gustaria multiplicar?:"));
+} while (multiplicando === Number) {
 
 for (i = 1; i <= 10; i++) {
 
@@ -6,4 +13,30 @@ for (i = 1; i <= 10; i++) {
     console.log(`${multiplicando}*${i} = ` + (multiplicando * i));
 
 }
+}
 
+
+let competidor=prompt("Ingrese su nombre")
+let edad=parseInt(prompt("Ingrese su edad"));
+
+while(competidor!=""){
+       
+    
+    switch(true){
+        case (edad>=16 && edad<=30):
+            alert(competidor+" "+"tiene"+" "+edad+" "+"e ingreso a la categoria jovenes");
+            break;
+        case (edad>=31 && edad<=50):
+            alert(competidor+" "+"tiene"+" "+edad+" "+"e ingreso a la categoria adultos");
+            break;
+        case (edad>=51 && edad<=65):
+            alert(competidor+" "+"tiene"+" "+edad+" "+"e ingreso a la categoria mayores");
+            break;
+        default:
+            alert(competidor+" "+" no puede ingresar a la carrera");
+            break            
+    }
+    
+    
+    
+}
